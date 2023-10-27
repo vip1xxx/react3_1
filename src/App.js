fe7d3bfd995cc00da22079cd0e88b4e87f16c626
x <fedorenkomcbook@gmail.com>
2023-10-27 16:57:07 +0200

    pushing

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {UsersContainer} from "./components/UsersContainer/UsersContainer";
+import {CommentsContainer} from "./components/CommentsContainer/CommentsContainer";
+
+const App = () => {
+  return (
+      <div style={{display: 'flex'}}>
+        <UsersContainer/>
+        <CommentsContainer/>
+      </div>
+  );
+};
+
+export {App};
